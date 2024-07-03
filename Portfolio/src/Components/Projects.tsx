6eb13d3e5d8c8f7b282a3e22197e5711cc8e8730
Austin <68400913+AustinK-Code@users.noreply.github.com>
2024-07-03 13:06:40 -0500

    Added portfolio to projects

--- Portfolio/src/Components/Projects.tsx
+++ Portfolio/src/Components/Projects.tsx
@@ -9,15 +9,16 @@ import {
   styled,
 } from "@mui/material";
 import GameHub from "../assets/GameHub.png";
+import Portfolio from "../assets/AKFullyLoaded.png"
 
 const Projects = () => {
   return (
     <>
       <Paper elevation={8} sx={{ borderRadius: 4 }}>
-        <Stack spacing={5} sx={{margin:8, marginTop:2}}>
+        <Stack spacing={5} sx={{ margin: 8, marginTop: 2 }}>
           {/* writing at the top */}
-            <Typography variant="h2">Projects</Typography>
-          {/* Project Card */}
+          <Typography variant="h2">Projects</Typography>
+          {/* Project Card Game Hub */}
           <Paper elevation={8} sx={{ backgroundColor: "#212121" }}>
             <Grid container spacing={2} alignItems="center">
               {/* Description side */}
@@ -41,17 +42,51 @@ const Projects = () => {
               </Grid>
               {/* Picture */}
               <Grid item xs={8}>
-                <Box
-                  sx={{ margin: 4}}
-                >
+                <Box sx={{ margin: 4 }}>
                   <img
-                   border-radius="50%"
+                    border-radius="50%"
                     src={GameHub}
                     alt="game hub site"
                     height="100%"
                     width="100%"
                   />
                 </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+          {/* Project Card AK Fully Loaded */}
+          <Paper elevation={8} sx={{ backgroundColor: "#212121" }}>
+            <Grid container spacing={2} alignItems="center">
+              {/* Picture */}
+              <Grid item xs={8}>
+                <Box sx={{ margin: 4 }}>
+                  <img
+                    border-radius="50%"
+                    src={Portfolio}
+                    alt="game hub site"
+                    height="100%"
+                    width="100%"
+                  />
+                </Box>
+              </Grid>{" "}
+              {/* Description side */}
+              <Grid item xs={4}>
+                <Container>
+                  <Typography variant="h5">AK Fully Coded</Typography>
+                  <Typography variant="body1" sx={{ pt: 1 }}>
+                    My portfolio site to showcase my projects and skills. Built
+                    with React, Material-UI, and Vite. Deployed on Vercel.
+                    <br />
+                    <Link
+                      href="https://game-hub-six-eta.vercel.app/"
+                      target="_blank"
+                      variant="inherit"
+                      underline="hover"
+                    >
+                      Here is a link
+                    </Link>
+                  </Typography>
+                </Container>
               </Grid>
             </Grid>
           </Paper>
