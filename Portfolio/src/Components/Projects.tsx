9c0658af555b05a5fcaee0e65e887a3e0c7a5388
Austin <68400913+AustinK-Code@users.noreply.github.com>
2024-07-03 12:58:11 -0500

    Finished Projects

--- Portfolio/src/Components/Projects.tsx
+++ Portfolio/src/Components/Projects.tsx
@@ -1,30 +1,60 @@
-import { Box, Container, Grid, Paper, Stack, Typography } from "@mui/material";
-
+import {
+  Box,
+  Container,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  Typography,
+  styled,
+} from "@mui/material";
+import GameHub from "../assets/GameHub.png";
 
 const Projects = () => {
   return (
     <>
-      <Paper elevation={8} sx={{borderRadius:4}}>
-        <Stack spacing={5}>
-          <Box sx={{ mb: 4 }}>
+      <Paper elevation={8} sx={{ borderRadius: 4 }}>
+        <Stack spacing={5} sx={{margin:8, marginTop:2}}>
+          {/* writing at the top */}
             <Typography variant="h2">Projects</Typography>
-            <Typography variant="body1" sx={{ pt: 1 }}>
-              Here are some of the projects I have worked on:
-            </Typography>
-          </Box>
-          <Grid container spacing={2}>
-            <Grid md={6}>
-              <Container>
-                <Typography variant="h5">Project 1</Typography>
-                <Typography variant="body1" sx={{ pt: 1 }}>
-                  Description of Project 1
-                </Typography>
-              </Container>
+          {/* Project Card */}
+          <Paper elevation={8} sx={{ backgroundColor: "#212121" }}>
+            <Grid container spacing={2} alignItems="center">
+              {/* Description side */}
+              <Grid item xs={4}>
+                <Container>
+                  <Typography variant="h5">Game Hub</Typography>
+                  <Typography variant="body1" sx={{ pt: 1 }}>
+                    A videogame discovery application using React, Chakra UI,
+                    Vite, and the RAWG.io API. Deployed on Vercel.
+                    <br />
+                    <Link
+                      href="https://game-hub-six-eta.vercel.app/"
+                      target="_blank"
+                      variant="inherit"
+                      underline="hover"
+                    >
+                      Here is a link
+                    </Link>
+                  </Typography>
+                </Container>
+              </Grid>
+              {/* Picture */}
+              <Grid item xs={8}>
+                <Box
+                  sx={{ margin: 4}}
+                >
+                  <img
+                   border-radius="50%"
+                    src={GameHub}
+                    alt="game hub site"
+                    height="100%"
+                    width="100%"
+                  />
+                </Box>
+              </Grid>
             </Grid>
-            <Grid md={6}>
-              <Paper elevation={3}><img src="src\assets\GameHub.png" alt="game hub site" height="100%" width="100%" /></Paper>
-            </Grid>
-          </Grid>
+          </Paper>
         </Stack>
       </Paper>
     </>
